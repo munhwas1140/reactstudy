2fcf67db472bd5c8b3a445cc8fb97e273d56c09e
Hyeong Jun Noh <munhwas1140@naver.com>
2023-12-29 00:44:56 +0900

    2023-12-29

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import TextInputWithFocusButton from './chapter_07/TextInputWithFocusButton';
+import Accommodation from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // setInterval(() => {
@@ -19,7 +24,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // }, 1000)
 root.render(
   <React.StrictMode>
-    <CommentList />
+    {/* <CommentList /> */}
+    {/* <NotificationList /> */}
+    {/* <TextInputWithFocusButton /> */}
+    {/* <Accommodation /> */}
+    {/* <ConfirmButton /> */}
+    <LandingPage />
   </React.StrictMode>
 );
 
